2034214511a90051f1615ce2c80cacda06772858
senaerdem <snnrrdm@gmail.com>
2023-09-26 21:29:16 +0300

    Creating Custom Subclasses for Reusability

---
+++ InstagramCloneApp/InstagramCloneApp/View/CustomTextField.swift
@@ -0,0 +1,33 @@
+//
+//  CustomTextField.swift
+//  InstagramCloneApp
+//
+//  Created by Sena Nur Erdem on 26.09.2023.
+//
+
+import UIKit
+
+class CustomTextField: UITextField {
+    
+    init(placeholder: String) {
+        super.init(frame: .zero)
+        
+        let spacer = UIView()
+        spacer.setDimensions(height: 50, width: 12)
+        leftView = spacer
+        leftViewMode = .always
+        
+        borderStyle = .none
+        textColor = .white
+        keyboardAppearance = .dark
+        keyboardType = .emailAddress
+        backgroundColor = UIColor(white: 1, alpha: 0.1)
+        setHeight(50)
+        attributedPlaceholder = NSAttributedString(string: placeholder, attributes: [.foregroundColor: UIColor(white: 1, alpha: 0.7)])
+        isSecureTextEntry = true
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
