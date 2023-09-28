d90d707c3c92f58968d99a18e940a4671a0ca5d8
senaerdem <snnrrdm@gmail.com>
2023-09-28 19:53:39 +0300

    Registration Page UI

--- InstagramCloneApp/InstagramCloneApp/Controller/Authentication/RegistrationController.swift
+++ InstagramCloneApp/InstagramCloneApp/Controller/Authentication/RegistrationController.swift
@@ -11,11 +11,80 @@ class RegistrationController: UIViewController {
     
     // MARK: - Properties
     
+    private let plushPhotoButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(imageLiteralResourceName: "plus_photo"), for: .normal)
+        button.tintColor = .white
+        return button
+    }()
+    
+    private let emailTextField: CustomTextField = {
+        let tf = CustomTextField(placeholder: "Email")
+        tf.keyboardType = .emailAddress
+        return tf
+    }()
+    
+    private let passwordTextField: UITextField = {
+        let tf = CustomTextField(placeholder: "Password")
+        tf.isSecureTextEntry = true
+        return tf
+    }()
+    
+    private let fullnameTextField = CustomTextField(placeholder: "Fullname")
+    private let usernameTextField = CustomTextField(placeholder: "Username")
+    
+    private let signUpButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Sign Up", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .systemPurple
+        button.layer.cornerRadius = 5
+        button.setHeight(50)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        return button
+    }()
+    
+    private let alreadyHaveAccountButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.attributedTitle(firstPart: "Already have an account? ", secondPart: "Sign In")
+        button.addTarget(self, action: #selector(handleShowLogin), for: .touchUpInside)
+        return button
+    }()
+    
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemPink
+        configureUI()
+    }
+    
+    // MARK: - Actions
+    
+    @objc func handleShowLogin() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    // MARK: - Helpers
+    
+    func configureUI() {
+        configureGradientLayer()
+        
+        view.addSubview(plushPhotoButton)
+        plushPhotoButton.centerX(inView: view)
+        plushPhotoButton.setDimensions(height: 140, width: 140)
+        plushPhotoButton.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 32)
+        
+        let stack = UIStackView(arrangedSubviews: [emailTextField, passwordTextField, fullnameTextField, usernameTextField, signUpButton])
+        
+        stack.axis = .vertical
+        stack.spacing = 20
+        
+        view.addSubview(stack)
+        stack.anchor(top: plushPhotoButton.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 32, paddingLeft: 32, paddingRight: 32)
+        
+        view.addSubview(alreadyHaveAccountButton)
+        alreadyHaveAccountButton.centerX(inView: view)
+        alreadyHaveAccountButton.anchor(bottom: view.safeAreaLayoutGuide.bottomAnchor)
     }
 }
 
